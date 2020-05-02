fa15a561dfd828ddb1485351fc930522a34fb2fa
hichotech <hichoh00@gmail.com>
2020-05-01 20:22:19 -0400

    add team 1 javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,26 @@
 let resetcontainer = document.querySelector("#reset-container");
-let resetsnum = document.querySelector ("#num-resets")
+let resetsnum = document.querySelector ("#num-resets");
+let team1goals = document.querySelector ("#teamone-numgoals");
+let team1shotsnum = document.querySelector ("#teamone-numshots"); 
+let team1shot = document.querySelector ("#teamone-shoot-button");
+
+
 
 
 resetcontainer.addEventListener ("click", function (){
-    
-    
     let newresetsnum = Number(resetsnum.innerHTML) + 1;
     resetsnum.innerHTML= newresetsnum;
+})
+
+team1shot.addEventListener ("click", function (){
+    if (Math.random() * 100 < 70){
+        let newteam1goals = Number(team1goals.innerHTML) + 1;
+    team1goals.innerHTML= newteam1goals;
+
+    }
+    let newteam1shotsnum = Number(team1shotsnum.innerHTML) + 1;
+    team1shotsnum.innerHTML= newteam1shotsnum;
+    
+    
 })
 
